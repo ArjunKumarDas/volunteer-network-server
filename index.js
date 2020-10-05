7263e08a981a0ebe05e5e9e74620795919f9d737
ArjunKumarDas <arjunkdinfo@gmail.com>
2020-10-05 14:15:12 +0600

    set up

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://vlunteer123:arjun7686@cluster0.on0y8.mongodb.net/vlunteerNetwork?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("vlunteerNetwork").collection("userInfo");
+  console.log('abc')
+});
+
 app.listen(port)
