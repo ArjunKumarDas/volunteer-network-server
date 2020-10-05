eacd1b376b7def6c8c3277b84e73b8b5cbec4328
ArjunKumarDas <arjunkdinfo@gmail.com>
2020-10-05 23:04:55 +0600

    root

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser')
 var cors = require('cors')
 const express = require('express')
-const port = 4000
+
 const app = express()
 app.use(bodyParser.json());
 app.use(cors());
-
+const port = 4000
+app.get('/', (req, res) => {
+  res.send("hello");
+})
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.on0y8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
