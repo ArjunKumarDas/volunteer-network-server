d1074572d45c505c77dadfccaa0b7c922d1dc023
ArjunKumarDas <arjunkdinfo@gmail.com>
2020-10-05 21:04:00 +0600

    done

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-
-
-
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://vlunteer123:arjun7686@cluster0.on0y8.mongodb.net/vlunteerNetwork?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -37,8 +34,6 @@ app.get('/allUser', (req, res) => {
 })
 
 });
-
-
 
 
 app.listen(port)
