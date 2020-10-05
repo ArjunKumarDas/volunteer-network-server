353f0d9ecb13842c6a9fbf5e84d098343074b80e
ArjunKumarDas <arjunkdinfo@gmail.com>
2020-10-05 19:35:07 +0600

    basic

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const bodyParser = require('body-parser')
+var cors = require('cors')
 const express = require('express')
-const app = express()
 const port = 4000
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
 
 
 const MongoClient = require('mongodb').MongoClient;
@@ -12,7 +15,23 @@ const uri = "mongodb+srv://vlunteer123:arjun7686@cluster0.on0y8.mongodb.net/vlun
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("vlunteerNetwork").collection("userInfo");
-  console.log('abc')
+  app.post('/addUser', (req, res) => {
+    const register = req.body;
+    collection.insertOne(register)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  })
+app.get('/user', (req, res) => {
+  collection.find({email: req.query.email})
+  .toArray((err, documents) => {
+    res.send(documents);
+  })
+})
+
 });
+
+
+
 
 app.listen(port)
