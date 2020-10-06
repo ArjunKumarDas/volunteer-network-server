e2fdb37d3991226b36fcf611a40fe7b6e599e23f
ArjunKumarDas <arjunkdinfo@gmail.com>
2020-10-06 11:23:15 +0600

    last

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 4000
 app.get('/', (req, res) => {
-  res.send("hello");
+  res.send("Volunteer Network server site");
 })
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.on0y8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
